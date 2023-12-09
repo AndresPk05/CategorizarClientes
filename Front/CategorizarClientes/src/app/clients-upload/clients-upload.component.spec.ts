9aef44fbc518cf98f1b8f2aa2f99fbb97ba3fb7c
Andres parrado <parrado.andresb@gmail.com>
2023-12-08 20:07:17 -0500

    [Commit]:"Se configuran pruebas unitarias de front y back"

--- Front/CategorizarClientes/src/app/clients-upload/clients-upload.component.spec.ts
+++ Front/CategorizarClientes/src/app/clients-upload/clients-upload.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClientsUploadComponent } from './clients-upload.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('ClientsUploadComponent', () => {
   let component: ClientsUploadComponent;
@@ -8,10 +10,10 @@ describe('ClientsUploadComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ClientsUploadComponent]
+      imports: [ClientsUploadComponent, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(ClientsUploadComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- Front/CategorizarClientes/src/app/model.service.spec.ts
+++ Front/CategorizarClientes/src/app/model.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ModelService } from './model.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ModelService', () => {
   let service: ModelService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ModelService);
   });
 
